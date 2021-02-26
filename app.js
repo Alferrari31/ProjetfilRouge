1e1b530e51a0fb013fec25cc8c7bee9e2c0bc023
Alferrari31 <68002914+Alferrari31@users.noreply.github.com>
2021-02-26 09:09:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(' .burger');
+const menu = document.querySelector('nav');
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('menu-open');
+
+}
+)
